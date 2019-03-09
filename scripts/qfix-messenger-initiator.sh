7202ec631420e2559aceb507dc70387fb82dffdd
huydangquoc <huysamdua@yahoo.com>
2019-03-09 11:05:46 +0700

    update start initiator script

--- scripts/qfix-messenger-initiator.sh
+++ scripts/qfix-messenger-initiator.sh
@@ -3,4 +3,4 @@
 CLASSPATH=cfg/initiator
 CLASSPATH=$CLASSPATH:lib/*
 
-java -cp "$CLASSPATH" com.jramoyo.qfixmessenger.QFixMessenger "cfg/initiator/messenger.cfg" "cfg/initiator/quickfix.cfg"
+java -cp "$CLASSPATH" com.jramoyo.qfixmessenger.QFixMessenger "cfg/initiator/messenger.cfg" "cfg/initiator/qfix-client-local-mkt.cfg"
